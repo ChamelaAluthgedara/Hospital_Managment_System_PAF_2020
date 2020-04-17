8ca26dfdd6a06ebe4ff92716e8ec883d6b56dbcf
Pasanrc <Pasanrc@DESKTOP-D8FCUJ6>
2020-04-17 21:34:49 +0530

    All APIs are working well.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,8 +2,13 @@ package controller;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,15 +22,44 @@ public class Controller {
 	HospitalRepo repo = new HospitalRepo();
 	
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Hospital> getAllHospitals() {
     	
         return repo.getAllHospitals();
     	
-        
-	
-	
+      }
+    
+    //Retrieve a data of a specific hospital
+    
+    @GET
+    @Path("get/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Hospital getHospital( @PathParam("id") int id ) {
+    	return repo.getHospital(id);
     }
+    
+    
+    @POST
+    @Path("add")
+    public void addHospital(Hospital hos) {
+    	System.out.println(hos);
+    	repo.addHospital(hos);
+    }
+    
+    @PUT
+    @Path("update")
+    public void updateHospital(Hospital hospital) {
+    	repo.updateHospital(hospital);
+    }
+    
+    
+    @DELETE
+    @Path("remove/{id}")
+    public void removeHospital(@PathParam("id") int id) {
+    	repo.removeHospital(id);
+    }
+    
+    
     
 	
 	
--- src/main/java/repo/HospitalRepo.java
+++ src/main/java/repo/HospitalRepo.java
@@ -2,6 +2,8 @@ package repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.json.bind.Jsonb;
+
 import models.Hospital;
 
 import java.sql.*;
@@ -62,9 +64,9 @@ public class HospitalRepo {
 	
 	
 	
-	public List<Hospital> getHospital(int id){ // Returns a specific object
-		List<Hospital> hosList = new ArrayList<Hospital>();
-		
+	public Hospital getHospital(int id){ // Returns a specific object
+		Hospital hospital = new Hospital();
+	
 		String QUERY = "SELECT * FROM Hospitals WHERE hosId = " + id;
 		
 		try {
@@ -72,21 +74,20 @@ public class HospitalRepo {
 			ResultSet rs = st.executeQuery(QUERY);
 			
 			if( rs.next() ) {
-				Hospital hos = new Hospital();
 				
-				hos.setId( rs.getInt(1) );
-				hos.setName( rs.getString(2) );
-				hos.setAddress( rs.getString(3) );
-				hos.setContactNum( rs.getInt(4) );
-				hos.setCharge( rs.getDouble(5) );
 				
-				hosList.add(hos); // Adding hospital objects
+				hospital.setId( rs.getInt(1) );
+				hospital.setName( rs.getString(2) );
+				hospital.setAddress( rs.getString(3) );
+				hospital.setContactNum( rs.getInt(4) );
+				hospital.setCharge( rs.getDouble(5) );
+				
 			}
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
-		return hosList;
+		return hospital;
 	}
 	
 	
@@ -94,6 +95,7 @@ public class HospitalRepo {
 	public void addHospital(Hospital hospital) {
 		
 		String QUERY = "INSERT INTO Hospitals() VALUES(?,?,?,?,?)";
+		
 		
 		try {
 			PreparedStatement st = conn.prepareStatement(QUERY);
@@ -113,6 +115,45 @@ public class HospitalRepo {
 			System.out.println(e);
 		}
 	}
+	
+	
+	
+	public void updateHospital(Hospital hospital) {
+		
+		String QUERY = "UPDATE Hospitals SET hosId = ?,hosName = ?, address = ?, contNum = ?, hosCharges = ?";
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(QUERY);
+			st.setInt(1, hospital.getHospitalId());
+			st.setString(2, hospital.getHospitalName());
+			st.setString(3, hospital.getAddress());
+			st.setInt(4, hospital.getContactNum());
+			st.setDouble(5, hospital.getCharges());
+			st.executeUpdate();
+			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+	public void removeHospital(int id) {
+		String QUERY = "DELETE FROM Hospitals WHERE id = ?";
+		try {
+			PreparedStatement st = conn.prepareStatement(QUERY);
+			st.setInt(1, id);
+			st.executeUpdate();
+			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		
+	}
+	
+	
 	
 	
 }
